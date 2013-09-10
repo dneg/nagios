b2b2c454d235c25c6397868a1577970e88071565
James Braid <jb@dneg.com>
2013-09-11 00:04:45 +0100

    add disk pool capacity thing, remove debug

--- Linux/EL6/etc/sensu/plugins/dneg/check_engenio_raid.pl
+++ Linux/EL6/etc/sensu/plugins/dneg/check_engenio_raid.pl
@@ -125,7 +125,7 @@ sub check_health {
     # fall through to failures - doesn't appear to be a health status = failed. line
     elsif (grep { /^The following failures have been found/ } @output) {
         $n->add_message(CRITICAL, 'storage array has failures');
-        print Dumper @output;
+        #print Dumper @output;
         if (grep { /^Volume - Hot Spare In Use/ } @output) {
             $n->add_message(CRITICAL, 'hot spare in use');
         }
@@ -134,6 +134,9 @@ sub check_health {
         }
         if (grep { /^Bypassed Drive/ } @output) {
             $n->add_message(CRITICAL, 'bypassed drive');
+        }
+        if (grep { /^Disk Pool Capacity - Warning Threshold Exceeded/ } @output) {
+            $n->add_message(CRITICAL, 'disk pool capacity');
         }
         
     }
@@ -156,7 +159,7 @@ sub run_sm_cli {
 
     # -S = supress informational messages (parsing command, running command, command success, etc)
     my $cmd = sprintf '%s %s -S -c "%s"', $sm_cli, $hostname, $command;
-    print "running $cmd\n";
+    #print "running $cmd\n";
     my @lines = split /\n/, `$cmd`;
 
     my $ret = ($? >> 8);

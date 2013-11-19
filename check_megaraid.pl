4aa11bf132a34b8a78ad918c03a045a8ff63fa7a
Johan van den Dorpe <jvd@dneg.com>
2013-11-19 13:04:24 +0000

    Return UNKNOWN if megaraid command doesn't succeed

--- check_megaraid.pl
+++ check_megaraid.pl
@@ -4,9 +4,13 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-my $megaraid = "/apps/Linux64/bin/megasasctl";
+my $megaraid = "bash -c /apps/Linux64/bin/megasasctl";
 
-my @output = `$megaraid`;
+my @output = `$megaraid 2>&1`;
+
+if ($? ne 0) {
+  unknown($output[0]);
+}
 
 foreach my $line (@output) {
 
@@ -32,4 +36,10 @@ sub critical {
   my $message = shift;
   print "CRITICAL: $message\n";
   exit 2;
+}
+
+sub unknown {
+  my $message = shift;
+  print "UNKNOWN: $message\n";
+  exit 3;
 }

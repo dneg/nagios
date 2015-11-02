351d16bd52e7dd6925ad35f4ff1d469580a4ed7b
James Braid <jb@dneg.com>
2015-11-02 13:26:01 +0000

    return separate errors for http / XML parsing errors

--- check_pa_config_sync.pl
+++ check_pa_config_sync.pl
@@ -39,14 +39,19 @@ $n->nagios_exit(OK, "");
 sub run_sync_status_check {
     my $palo_alto = $n->opts->hostname;
     my $api_key = $n->opts->key;
+
     # construct palo alto xml api request url
     my $url = sprintf("https://%s/api/?type=op&key=%s&cmd=%s", $palo_alto, $api_key, uri_escape("<show><high-availability><state></state></high-availability></show>"));
 
     # get XML output from palo alto xml api request
-    my $states = XMLin(get($url));
-    $n->nagios_exit(CRITICAL, "No results returned from palo altos. Is the host dead? Wrong API key?") unless(defined($states));
+    my $resp = get($url);
+    $n->nagios_exit(CRITICAL, "No results returned from palo altos. Is the host dead? Wrong API key?") unless defined $resp;
+
+    my $states = XMLin($resp);
+    $n->nagios_exit(CRITICAL, "XML parsing failed") unless exists $states->{"result"}; 
     
     # check running sync config status in xml format
     my $sync_status = $states->{"result"}->{"group"}->{"running-sync"};
     $n->nagios_exit(CRITICAL, "Running-sync status on $palo_alto is not synchronized.") unless ($sync_status eq "synchronized");
+
 }
